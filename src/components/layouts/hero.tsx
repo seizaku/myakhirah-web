dd8fb2690b82e311b6345c8c38edbeb58bab758b
Zakusei <zakusei.dev@gmail.com>
2024-06-08 05:07:03 +0800

    feat: hero component

---
+++ src/components/layouts/hero.tsx
@@ -0,0 +1,8 @@
+export const Hero = () => (
+  <section className="flex h-48 w-full flex-col items-center justify-center rounded-lg bg-primary text-primary-foreground">
+    <h5 className="text-md mb-2 font-light">Woohoo!</h5>
+    <h1 className="text-center text-lg font-bold md:text-3xl">
+      Your authentication is all sorted. <br /> Build the important stuff
+    </h1>
+  </section>
+);
