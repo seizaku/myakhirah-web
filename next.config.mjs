f208b9bfb3897fbc42241dacc2ad71ef56419f41
Zakusei <zakusei.dev@gmail.com>
2024-06-08 04:20:49 +0800

    chore: temporary placeholder image

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["creatorspace.imgix.net", "images.ctfassets.net"],
+    domains: [
+      "creatorspace.imgix.net",
+      "images.ctfassets.net",
+      "storage.googleapis.com",
+    ],
   },
 };
 
