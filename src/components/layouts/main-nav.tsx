6d5256975f152394c8d02302f9b3d86ebdcbaa10
Zakusei <zakusei.dev@gmail.com>
2024-06-07 14:06:26 +0800

    feat: add main nav layout

---
+++ src/components/layouts/main-nav.tsx
@@ -0,0 +1,20 @@
+import { SITE_CONFIG } from "@/config/site-config";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+
+export const MainNav = () => (
+  <nav className="hidden sm:flex">
+    <ul className="flex gap-2">
+      {SITE_CONFIG.mainNav.map((item) => (
+        <li>
+          <Link
+            href={item.href}
+            className={buttonVariants({ variant: "ghost" })}
+          >
+            {item.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
