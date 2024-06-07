194ed670d818b98b9cf73a2aabbba8adb0fbe33b
Zakusei <zakusei.dev@gmail.com>
2024-06-08 05:32:27 +0800

    feat: footer icons

--- src/app/(user)/[username]/page.tsx
+++ src/app/(user)/[username]/page.tsx
@@ -1,4 +1,13 @@
 import prisma from "@/app/db/prisma";
+import { Button } from "@/components/ui/button";
+import {
+  MixerHorizontalIcon,
+  HeartFilledIcon,
+  RocketIcon,
+  GitHubLogoIcon,
+  PaperPlaneIcon,
+  Link2Icon,
+} from "@radix-ui/react-icons";
 
 interface UserPage {
   params: {
@@ -22,6 +31,28 @@ export default function UserPage({ params: { username } }: UserPage) {
           </div>
         </div>
       </div>
+      <nav className="container bottom-8 left-8 mb-4 text-muted-foreground xl:fixed">
+        <div className="flex items-center gap-4 divide-x">
+          <ul className="flex gap-2">
+            <li>
+              <Button variant={"ghost"} className="rounded-full" size={"icon"}>
+                <MixerHorizontalIcon className="h-4 w-4" />
+              </Button>
+            </li>
+            <li>
+              <Button variant={"ghost"} className="rounded-full" size={"icon"}>
+                <Link2Icon className="h-4 w-4" />
+              </Button>
+            </li>
+            <li>
+              <Button variant={"ghost"} className="rounded-full" size={"icon"}>
+                <HeartFilledIcon className="h-4 w-4" />
+              </Button>
+            </li>
+          </ul>
+          <p className="pl-6 text-xs">No views yesterday</p>
+        </div>
+      </nav>
     </main>
   );
 }
