b6c7de543a0d71f4f41c84182259be2d043e924d
Zakusei <zakusei.dev@gmail.com>
2024-06-08 21:11:12 +0800

    refactor: clean initial code

--- src/app/(user)/dashboard/page.tsx
+++ src/app/(user)/dashboard/page.tsx
@@ -1,9 +1,6 @@
 import { SiteHeader } from "@/components/layouts/site-header";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
-import { UsernameForm } from "./_components/username-form";
-import { ProfileForm } from "./_components/profile-form";
-import { Button } from "@/components/ui/button";
 import { Hero } from "@/components/layouts/hero";
 
 export default async function Dashboard() {
@@ -20,16 +17,10 @@ export default async function Dashboard() {
       <SiteHeader />
       <div className="container">
         <Hero />
-        <section className="mx-auto max-w-lg space-y-4 py-12">
-          <UsernameForm />
-          <ProfileForm />
-          <hr />
-          <Button className="w-full">Add Project</Button>
-        </section>
-        {/* <section className="py-12">
+        <section className="py-12">
           <h1>User Data</h1>
           <pre>{JSON.stringify(user, null, 2)}</pre>
-        </section> */}
+        </section>
       </div>
     </main>
   );
