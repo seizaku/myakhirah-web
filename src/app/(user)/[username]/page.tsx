1b68f2034698ed8111573b1bfbf1072775f82623
Zakusei <zakusei.dev@gmail.com>
2024-06-09 05:02:48 +0800

    feat: user page component with zustand states

--- src/app/(user)/[username]/page.tsx
+++ src/app/(user)/[username]/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import prisma from "@/db/prisma";
 import { Button } from "@/components/ui/button";
 import {
@@ -8,6 +9,7 @@ import {
   PaperPlaneIcon,
   Link2Icon,
 } from "@radix-ui/react-icons";
+import { BottomControl } from "@/components/control-cards";
 
 interface UserPage {
   params: {
@@ -17,20 +19,37 @@ interface UserPage {
 import { UserInfo } from "./_components/user-info";
 import { UserProjects } from "./_components/user-projects";
 
+import { useScreenView } from "@/stores/responsive-store";
+import { useEffect, useState } from "react";
+
 export default function UserPage({ params: { username } }: UserPage) {
+  const { isMobileView } = useScreenView();
+  const [isLoading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 800);
+  }, [isMobileView]);
+
   return (
-    <main className="min-h-screen">
-      <div className="container mx-auto h-full w-full max-w-[1728px] pt-16">
+    <main className={`min-h-screen ${isMobileView && !isLoading && "py-12"}`}>
+      <div
+        className={`container mx-auto h-full w-full bg-background duration-500 ease-in-out ${isMobileView && !isLoading ? "no-scrollbar max-h-[720px] max-w-md overflow-y-scroll rounded-3xl border-4" : "max-w-[1728px]"} ${isLoading && "opacity-0"} pt-16 transition-all`}
+      >
         <div className="relative mx-auto h-full w-fit xl:w-full">
-          <UserInfo />
+          <UserInfo isLoading={isLoading} />
         </div>
         <div className="flex h-full w-full flex-col pb-6 xl:flex-row">
           <div className="w-5/6"></div>
-          <div className="mx-auto h-full min-w-fit xl:w-full xl:max-w-full">
+          <div className="mx-auto h-full w-full xl:min-w-fit xl:max-w-full">
             <UserProjects />
           </div>
         </div>
       </div>
+
+      {/* Footer Menu */}
       <nav className="container bottom-8 left-8 mb-4 text-muted-foreground xl:fixed">
         <div className="flex items-center gap-4 divide-x">
           <ul className="flex gap-2">
@@ -53,6 +72,8 @@ export default function UserPage({ params: { username } }: UserPage) {
           <p className="pl-6 text-xs">No views yesterday</p>
         </div>
       </nav>
+
+      <BottomControl />
     </main>
   );
 }
