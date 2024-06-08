940bb21f58aef4e51c2d9cb3a1427e12bbd2739a
Zakusei <zakusei.dev@gmail.com>
2024-06-09 05:04:08 +0800

    feat: delete card component

--- src/components/delete-card.tsx
+++ src/components/delete-card.tsx
@@ -1,9 +1,14 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { TrashIcon } from "@radix-ui/react-icons";
+import { useUserProfile } from "@/stores/projects-store";
+import { Item } from "@/types";
 
-export const DeleteCardButton = () => {
+export const DeleteCardButton = ({ item }: { item: Item }) => {
+  const { deleteItem } = useUserProfile();
   return (
     <Button
+      onClick={() => deleteItem(item)}
       className="absolute -left-3 -top-3 rounded-full opacity-0 transition-opacity group-hover:opacity-100"
       variant={"outline"}
       size={"icon"}
